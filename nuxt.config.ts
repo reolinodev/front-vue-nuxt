0b2658012358e81e243407a8305b120733c91532
reolinodev <reolino.dev@gmail.com>
2024-01-10 17:39:51 +0900

    [feature] env 분기처리

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL
+    }
+  },
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css'
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,16 @@
 
 <script setup lang="ts">
 import { commonStore } from '~/stores/common'
+import { headerStore } from '~/stores/header'
 
 const common = commonStore()
+const header = headerStore()
 
 const router = useRouter()
 if (!common.isLogin) {
   router.push('login')
+} else {
+  header.navigatorNm = 'Main'
 }
 </script>
 
